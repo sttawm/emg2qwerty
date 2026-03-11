fafe5dcd6c5375a4b18e11f12b331984e58389a6
sttawm <mmww88wwmm@gmail.com>
2026-03-11 15:30:29 -0700

    resume lstm bidirectional from checkpoint

---
+++ cmd.sh
@@ -0,0 +1,5 @@
+  python train_remote.py \
+    --spot \
+    --experiment lstm_bidirectional_1x256+cosine_lr+no_dropout_resumed \
+    checkpoint=gs://emg2qwerty-team-logs/logs/lstm_bidirectional_1x256+cosine_lr+no_dropout/checkpoints/last.ckpt
+  
